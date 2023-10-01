6ab9f1be5d3de722eb3a76c2eafdb7625090eacc
herbert <h.beemster@nki.nl>
2023-10-01 11:30:07 +0200

    - Applied some refactorings

--- src/requests_rest_api/__about__.py
+++ src/requests_rest_api/__about__.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2023-present herbert <h.beemster@nki.nl>
+# SPDX-FileCopyrightText: 2023-present herbert <dataengineer42@gmail.com>
 #
 # SPDX-License-Identifier: MIT
 __version__ = "0.0.1"
--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,3 @@
-# SPDX-FileCopyrightText: 2023-present herbert <h.beemster@nki.nl>
+# SPDX-FileCopyrightText: 2023-present herbert <dataengineer42@gmail.com>
 #
 # SPDX-License-Identifier: MIT
